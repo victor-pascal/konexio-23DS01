1c6bec816f33674ae3c847d44a517bce3c15e4f4
vpascal <vpascal@altaprofits.fr>
2023-02-16 16:28:01 +0100

    Correction session 3

---
+++ session3/1-comparateurs/main.js
@@ -0,0 +1,20 @@
+/* LES COMPARATEURS */
+console.log('Bienvenue dans la console javascript');
+console.log('Vous êtes dans les exercices sur les comparateurs');
+
+// ------ TODO 1 ------ 
+// 1. Créer une variable neighbourAge qui contient l'âge de votre voisin
+var neighbourAge = 20;
+
+// 2. Créer une variable myAge qui contient votre âge
+var myAge = 17;
+
+// 3. Comparez les variables neighbourAge et monAge en utilisant des comparateurs (<, <=, >, >=, ===, !== ...)
+// puis affichez le résultat des comparaisons dans la console avec la méthode "console.log".
+
+console.log(neighbourAge < myAge);
+console.log(neighbourAge <= myAge);
+console.log(neighbourAge > myAge);
+console.log(neighbourAge >= myAge);
+console.log(neighbourAge === myAge);
+console.log(neighbourAge !== myAge);
---
+++ session3/2-conditions/main.js
@@ -0,0 +1,19 @@
+/* LES CONDITIONS */
+console.log('Bienvenue dans la console javascript');
+console.log('Vous êtes dans les exercices sur les conditions');
+
+// ------ TODO 1 ------ 
+// 1. Créer une variable currentColor qui contient la valeur "green"
+var currentColor = "green";
+
+// 2. Comparez la variable "currentColor" avec la valeur "red" (c'est une string), en utilisant une condition (if) et des comparateurs (=== ou !==) :
+// - si la variable "currentColor" est égale à "red", affichez "fraise" dans la console
+// - sinon, affichez "un fruit d'une couleur" dans la console
+
+//3. Jouez avec la valeur de la variable currentColor, pour rentrer dans l'une ou l'autre condition
+
+if (currentColor === "red") {
+    console.log("fraise");
+} else {
+    console.log("un fruit d'une couleur : " + currentColor);
+}
